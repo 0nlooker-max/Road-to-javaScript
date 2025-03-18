c93f5843ed9e901b7e8092a21473ad7bb8f4a516
Query <rexpagatpat@gmail.com>
2025-03-18 14:10:45 +0800

    centerde

--- delete.php
+++ delete.php
@@ -1,20 +1,20 @@
 <?php
-header('Content-Type: application/json');
+    header('Content-Type: application/json');
 
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
+    error_reporting(E_ALL);
+    ini_set('display_errors', 1);
 
-include("connection.php");
+    include("connection.php");
 
-$studentId = $_POST['id'];
+    $studentId = $_POST['id'];
 
-$query = "DELETE FROM prilimtable WHERE student_id = ?";
-$statement = $connection->prepare($query);
-$res = $statement->execute([$studentId]);
+    $query = "DELETE FROM prilimtable WHERE student_id = ?";
+    $statement = $connection->prepare($query);
+    $res = $statement->execute([$studentId]);
 
-if($res){
-    echo json_encode(["res" => "success"]);
-}else{
-    echo json_encode(["res" => "error", "msg" => "Student not deleted"]);
-}
+    if($res){
+        echo json_encode(["res" => "success"]);
+    }else{
+        echo json_encode(["res" => "error", "msg" => "Student not deleted"]);
+    }
 ?>
